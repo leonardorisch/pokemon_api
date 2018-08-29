40bc2ac7c03c09af12a388121a1845075ef7095a
Leonardo <leo.mrisch@gmail.com>
2018-08-29 18:57:51 -0300

    add more specs checking status

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ApiController do
-  describe "#show_pokemon" do
+  describe "GET #show_pokemon" do
     let(:pokemon) { create(:pokemon) }
-    let(:valid_params) do
+    let(:expected_valid_params) do
       {
         id: pokemon.id,
         name: pokemon.name,
@@ -17,12 +17,26 @@ RSpec.describe ApiController do
         speed: pokemon.speed,
         generation: pokemon.generation,
         legendary: pokemon.legendary
-      }.to_json
+      }.with_indifferent_access
     end
+
     context "when request is made with valid params" do
-      it do
-        get :show_pokemon, params: { id: pokemon.id, format: :json }
-        expect(response.body).to eq(valid_params)
+      before { get :show_pokemon, params: { id: pokemon.id, format: :json } }
+      it "get a valid pokemon" do
+        expect(JSON.parse(response.body)).to include(expected_valid_params)
+      end
+      it "return status 200" do
+        expect(JSON.parse(response.code)).to eq(200)
+      end
+    end
+
+    context "when request is made with invalid pokemon" do
+      before { get :show_pokemon, params: { id: '777', format: :json } }
+      it "return 404" do
+        expect(JSON.parse(response.code)).to eq(404)
+      end
+      it "return message pokemon not found" do
+        expect(response.body).to include("Pokemon not found!")
       end
     end
   end
