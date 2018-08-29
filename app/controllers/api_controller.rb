cca11a16863cd30a4f08a3450d507a22ecc5efc9
Leonardo <leo.mrisch@gmail.com>
2018-08-29 18:57:33 -0300

    refactor status methods to render with status_code correctly

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,17 +4,19 @@ class ApiController < ApplicationController
 
   def show_pokemon
     pokemon = Pokemon.find(params[:id]).attributes.except(*EXCEPTED_PARAMS)
-    render_pretty(pokemon)
+    render_pretty(pokemon, 200)
+  rescue ActiveRecord::RecordNotFound => e
+    render_pretty("Pokemon not found!", 404 )
   end
 
   def show_pokemons
     pokemons = Pokemon.all.map { |pokemon| pokemon.attributes.except(*EXCEPTED_PARAMS)  }
-    render_pretty(pokemons)
+    render_pretty(pokemons, status)
   end
 
   private
 
-  def render_pretty(content)
-    render json: JSON.pretty_generate(content)
+  def render_pretty(content, status)
+    render json: JSON.pretty_generate(content), status: status
   end
 end
