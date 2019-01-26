0abbeb44483af0e8d23ba6a6665561312363eb9a
Leonardo <leo.mrisch@gmail.com>
2019-01-26 15:16:57 -0200

    change rails to japronto to test micro framework

--- app/models/pokemon.rb
+++
@@ -1,5 +0,0 @@
-class Pokemon < ApplicationRecord
-  validates :name, :first_type, :second_type, presence: true
-  validates :strength, :hp, :attack, :defense, :special_atk, :speed, presence: true
-  validates :generation, :legendary, presence: true
-end
